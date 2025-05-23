2bcf774298de52fb63a7b9c4446c4b82ccdd68cc
Eric Bach <eric.bach@ama.ab.ca>
2025-05-23 13:13:17 -0600

    Remove router

--- frontend/app/den/5/page.tsx
+++ frontend/app/den/5/page.tsx
@@ -1,11 +1,8 @@
 'use client';
 
 import Image from 'next/image';
-import { useRouter } from 'next/navigation';
 
 export default function HomePage() {
-  const router = useRouter();
-
   return (
     <div className='relative w-full h-screen bg-gray-100 cursor-default'>
       <Image src='/images/den/5.jpg' alt='Slide 5' fill className='object-contain' priority sizes='100vw' />
