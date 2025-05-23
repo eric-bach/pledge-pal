a600c2c320b2d3344c738e763bb8f5e5694047ce
Eric Bach <eric.bach@ama.ab.ca>
2025-05-23 13:12:52 -0600

    Updates to pages

--- frontend/app/den/1/page.tsx
+++ frontend/app/den/1/page.tsx
@@ -8,7 +8,7 @@ export default function HomePage() {
 
   return (
     <div className='relative w-full h-screen bg-gray-100 cursor-default' onClick={() => router.push('/den/2')}>
-      <Image src='/images/den/1.jpeg' alt='Slide 1' fill className='object-contain' priority sizes='100vw' />
+      <Image src='/images/den/1.jpg' alt='Slide 1' fill className='object-contain' priority sizes='100vw' />
     </div>
   );
 }
--- frontend/app/den/2/page.tsx
+++ frontend/app/den/2/page.tsx
@@ -1,5 +1,14 @@
 'use client';
 
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+
 export default function HomePage() {
-  return <div>Slide 2</div>;
+  const router = useRouter();
+
+  return (
+    <div className='relative w-full h-screen bg-gray-100 cursor-default' onClick={() => router.push('/den/3')}>
+      <Image src='/images/den/2.jpg' alt='Slide 2' fill className='object-contain' priority sizes='100vw' />
+    </div>
+  );
 }
